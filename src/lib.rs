88f5babb6230d5b785bbee8284140d5351f5d285
James Hannah <jhannah8124@gmail.com>
2015-03-10 19:36:46 -0700

    inital build of clear function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+extern crate term;
+
+///Clears the screen using terminfo-based clear string
+pub fn clear() {
+    let info = term::terminfo::TermInfo::from_env();
+
+    let fred = match info {
+        Ok(val) => val,
+        Err(f) => panic!(f.to_string()),
+    };
+
+    let clear = fred.strings.get("clear").unwrap();
+    print!("{}", std::str::from_utf8(clear).unwrap());
+}
+
 #[test]
 fn it_works() {
 }
